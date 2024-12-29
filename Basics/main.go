355a5820bdeb701efd7e9f36d4c659d88d97614e
Nithin1729S <sureshnithin1729@gmail.com>
2024-12-29 21:16:42 +0530

    Add Describer interface and implementations for Person and Car

--- Basics/main.go
+++ Basics/main.go
@@ -20,6 +20,32 @@ func announce(animal Animal){
 	fmt.Println(animal.GetInfor())
 }
 
+
+type Describer interface{
+	Describe() string
+}
+
+type Person struct{
+	Name string
+}
+
+func (p Person) Describe() string{
+	return fmt.Sprintf("I'm %s",p.Name)
+}
+
+type Car struct{
+	Model string
+}
+
+func(c Car) Describe() string{
+	return fmt.Sprintf("Model: %s",c.Model)
+}
+
+func findDescriber(d Describer){
+	fmt.Println(d.Describe())
+}
+
+
 func main(){
 	ppls:=[]string{"John","Doe","Nithin","Alaska"}
 	for _,ppl:=range ppls{
@@ -33,5 +59,11 @@ func main(){
 
 	c:=Cat{"Tom",5}
 	announce(c)
+
+	p:=Person{"John"}
+	findDescriber(p)
+
+	car:=Car{"BMW"}
+	findDescriber(car)
 	
 }
