57ac46572ba2c067eb9061336f277d9ab6846e47
Nithin1729S <sureshnithin1729@gmail.com>
2025-01-17 14:35:09 +0530

    simple bool implementation of bloom filter

--- Bloom FIlters/go.mod
+++ Bloom FIlters/go.mod
@@ -1,3 +1,5 @@
 module github.com/Nithin1729S/BloomFilters
 
 go 1.22.2
+
+require github.com/spaolacci/murmur3 v1.1.0 // indirect
--- Bloom FIlters/main.go
+++ Bloom FIlters/main.go
@@ -1,9 +1,53 @@
 package main
-import(
+
+import (
 	"fmt"
-)
+	"hash"
+	"time"
+	"github.com/spaolacci/murmur3"
+)	
+
+var mHasher hash.Hash32
+
+func init(){
+	mHasher = murmur3.New32WithSeed(uint32(time.Now().UnixNano()))
+}
+
+func murmurHash(key string,size int) int {
+	mHasher.Write([]byte(key))
+	result := mHasher.Sum32() % uint32(size)
+	mHasher.Reset()
+	return int(result)
+}
+
+type BloomFilter struct{
+	filter []bool
+	size int
+}
+
+func NewBloomFilter(size int) *BloomFilter{
+	return &BloomFilter{filter:make([]bool,size),size:size}
+}
+
+func (b *BloomFilter) Add(key string){
+	idx :=murmurHash(key,b.size) % b.size
+	b.filter[idx] = true
+}
+
+func (b *BloomFilter) Exists(key string) bool {
+	idx :=murmurHash(key,b.size) % b.size
+	return b.filter[idx]
+}
+
 
 func main(){
-	
-	fmt.Println("Hello World")
+	bloom:=NewBloomFilter(16)
+	keys:=[]string{"a","b","c"}
+	for _,key := range keys{
+		bloom.Add(key)
+	}
+	keys=append(keys,"d")
+	for _,key :=range keys{
+		fmt.Println(key,bloom.Exists(key))
+	}
 }
